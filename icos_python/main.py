224bce4e2b0c79379d0909e07eea0a5e279e1cab
Andrew Tugman <andrewtugman@Andrews-Air.lan>
2023-01-18 13:25:28 -0500

    Remove comments

--- icos_python/main.py
+++ icos_python/main.py
@@ -2,9 +2,6 @@
 
 import ibm_boto3
 from ibm_botocore.client import Config, ClientError
-#from ibm_s3transfer.aspera.manager import AsperaTransferManager
-#from ibm_s3transfer.aspera.manager import AsperaConfig
-import time
 import os
 
 # Example constants for IBM COS values
@@ -30,7 +27,8 @@ COS_INSTANCE_CRN = data[2].strip()
 COS_BUCKET_LOCATION = data[3].strip()
 
 ## Currently using local files and user input for demo files to upload to iCOS
-## Optionally you can set up files / file contents as variables as shown here
+## Optionally you can set up files / file contents as variables as shown here, and pass them as params
+
 # bucket_1 = <bucket_name>
 # bucket_2 = <bucket_2_name>
 
@@ -109,97 +107,16 @@ def get_item(bucket_name, item_name):
 
 def delete_item(bucket_name, object_name):
     try:
-        #cos.delete_object(Bucket=bucket_name, Key=object_name)
         cos.Object(bucket_name, object_name).delete()
         print("Item: {0} deleted!\n".format(object_name))
     except ClientError as be:
         print("CLIENT ERROR: {0}\n".format(be))
     except Exception as e:
         print("Unable to delete object: {0}".format(e))
-'''
-def add_protection_configuration_to_bucket(bucket_name):
-    try:
-        new_protection_config = {
-            "Status": "Retention",
-            "MinimumRetention": {"Days": 10},
-            "DefaultRetention": {"Days": 100},
-            "MaximumRetention": {"Days": 1000}
-        }
-        
-        #cos.put_bucket_protection_configuration(Bucket=bucket_name, ProtectionConfiguration=new_protection_config)
-        cos.put_bucket_protection_configuration(bucket_name, new_protection_config)
-        #cos.Bucket(bucket_name)
-
-        print("Protection added to bucket {0}\n".format(bucket_name))
-    except ClientError as be:
-        print("CLIENT ERROR: {0}\n".format(be))
-    except Exception as e:
-        print("Unable to set bucket protection config: {0}".format(e))
-
-def get_protection_configuration_on_bucket(bucket_name):
-    try:
-        response = cos.get_bucket_protection_configuration(Bucket=bucket_name)
-
-        protection_config = response.get("ProtectionConfiguration")
-
-        print("Bucket protection config for {0}\n".format(bucket_name))
-        print(protection_config)
-        print("\n")
-    except ClientError as be:
-        print("CLIENT ERROR: {0}\n".format(be))
-    except Exception as e:
-        print("Unable to get bucket protection config: {0}".format(e))
-
-def upload_file_with_retention(bucket_name, object_name, path_to_file, retention_period):
-    print("Uploading file {0} to object {1} in bucket {2}\n".format(path_to_file, object_name, bucket_name))
-    
-    args = {
-        "RetentionPeriod": retention_period
-    }
-
-    cos.upload_file(
-        Filename=path_to_file,
-        Bucket=bucket_name,
-        Key=object_name,
-        ExtraArgs=args
-    )
-
-    print("File upload complete to object {0} in bucket {1}\n".format(object_name, bucket_name))
-'''
-'''
-# Create Transfer manager
-def download_item(bucket_name, object_name, download_filename):
-
-    client = ibm_boto3.client("s3",
-        ibm_api_key_id=COS_API_KEY_ID,
-        ibm_service_instance_id=COS_INSTANCE_CRN,
-        config=Config(signature_version="oauth"),
-        endpoint_url=COS_ENDPOINT
-    )
-
-    with AsperaTransferManager(client) as transfer_manager:
-
-        # Get object with Aspera
-        future = transfer_manager.download(bucket_name, object_name, download_filename)
-
-        # Wait for download to complete
-        future.result()
-'''
-
-'''
-get_buckets()
-#create_bucket(bucket_2)
-#create_text_file(bucket_2,file_1,"some OTHER text that should be in the file!")
-get_bucket_contents(bucket_2)
-get_item(bucket_2, file_1)
-delete_item(bucket_2, file_1)
-get_item(bucket_2, file_1)
-'''
 
 program_running = True
 
 while program_running == True:
-    #user_input = input("What would you like to do?")
     print("###########################################################")
     print("Welcome to a simple Python app that leverages the iCOS API!")
     print("What iCOS operation would you like to perform?")
@@ -211,8 +128,6 @@ while program_running == True:
     print("4. Get bucket contents")
     print("5. Get item contents")
     print("6. Delete item")
-    #print("7. Add protection to existing bucket")
-    #print("8. Check protection level on existing bucket")
     print("\n")
     user_input = input("Select an option from the list above: ")
 
@@ -324,55 +239,3 @@ while program_running == True:
             pass
 
         print("\n")
-
-'''
-
-    elif user_input == "7":
-        user_input_7_2 = input("Bucket to add protection to? ")
-        add_protection_configuration_to_bucket(user_input_7_2)
-
-        user_input_7_3 = input("Press enter to return to main menu or type 'exit' to exit the program. ")
-
-        if user_input_7_3 == "":
-            pass
-        elif user_input_7_3 == "exit":
-            program_running = False
-        else:
-            pass
-
-        print("\n")
-
-    elif user_input == "8":
-        user_input_8_2 = input("Bucket to check protection on? ")
-        get_protection_configuration_on_bucket(user_input_8_2)
-
-        user_input_8_3 = input("Press enter to return to main menu or type 'exit' to exit the program. ")
-
-        if user_input_8_3 == "":
-            pass
-        elif user_input_8_3 == "exit":
-            program_running = False
-        else:
-            pass
-
-        print("\n")
-
-    elif user_input == "9":
-        user_input_9_2 = input("Bucket name? ")
-        user_input_9_3 = input("Object name? ")
-        user_input_9_4 = input("Path? ")
-        user_input_9_5 = input("Retention period? ")
-
-        upload_file_with_retention(user_input_9_2, user_input_9_3, user_input_9_4, user_input_9_5)
-
-        user_input_9_6 = input("Press enter to return to main menu or type 'exit' to exit the program. ")
-
-        if user_input_9_6 == "":
-            pass
-        elif user_input_9_6 == "exit":
-            program_running = False
-        else:
-            pass
-
-        print("\n")
-'''

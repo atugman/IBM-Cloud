0780870bf2221c4b30c214e92f8354986e644d10
Andrew Tugman <andrewtugman@Andrews-Air.lan>
2023-06-02 16:27:15 -0400

    Fix EOF script

--- Labs/lb-scripts/test_with_eof.sh
+++ Labs/lb-scripts/test_with_eof.sh
@@ -9,12 +9,6 @@ hostname -i > ip.txt
 sed 's/\s.*$//' ip.txt > ip_trimmed.txt
 inst_name=`cat inst_name.txt` # export?
 ip=`cat ip_trimmed.txt`
-echo "<head>
-<title>Load Balancer Lab</title>
-</head>
-<body>
-Traffic is hitting @@inst_name.txt@@ at @@ip_trimmed.txt@@" > index.html
-
 
 cat <<EOF > index.html
 <head>
@@ -22,48 +16,4 @@ cat <<EOF > index.html
 </head>
 <body>
 Traffic is hitting: <div class="span6">$(<inst_name.txt) at $(<ip_trimmed.txt)</div>
-
-    <form action="" id="webForm">
-        <h1></h1>
-        <input type="text" id="digit" class="form-control" placeholder="Enter a single digit 0-9"><br>
-        <h1></h1>
-        <button type="submit">Submit</button>
-      </form>
-    <div id="my-div"></div>
-    <div id="test-div"></div>
-
-    <script>
-
-        var checker = sessionStorage.getItem("digit")
-
-        if (checker) {
-            document.getElementById("my-div").innerHTML = `The current stored value is: ${checker}`
-        }
-        console.log(sessionStorage.getItem("digit"))
-
-        let webForm = document.getElementById("webForm");
-
-        webForm.addEventListener("submit", (e) => {
-        e.preventDefault();
-
-        let digit = document.getElementById("digit");
-
-        if (digit.value.length > 1) {
-            alert("Ensure your input is only a single digit!");
-        } else {
-
-        document.getElementById("my-div").innerHTML = `The current stored value is: ${digit.value}`
-
-            sessionStorage.setItem("digit", digit.value);
-
-            console.log(sessionStorage.getItem("digit"))
-            digit.value = "";
-        }
-        });
-    </script>
-</body>
 EOF
-#sed -e '/@@inst_name.txt@@/{inst_name.txt' -e ';d}' inst_name.html
-#sed -e '/@@ip_trimmed.txt@@/{ip_trimmed.txt' -e ';d}' ip.html
-#sed -e '/@@CONTENTS.txt@@/{rCONTENTS.txt' -e ';d}' inst_name.html > index.html
-#sed -e '/@@CONTENTS.txt@@/{rCONTENTS.txt' -e ';d}' ip.html > index.html
